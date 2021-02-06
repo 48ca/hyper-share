b87b0682128d8f4f2f29b3629292da6966307f97
James Houghton <jhoughton@virginia.edu>
2021-02-06 00:58:34 -0500

    Add some file upload integration tests

---
+++ integration_tests/run_tests.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -ue
+
+set -m # Enable jobs
+
+export PORT=12389
+export DIR=$(mktemp -d)
+export BOUNDARY="aaaaaaaaaaaaaaaaaaaa" # 20 a's
+
+echo "Starting hypershare"
+
+cargo build
+cargo run -- -d $DIR -p $PORT -h "127.0.0.1" -u --headless | sed -e 's/^/ >>> /g' &
+
+sleep 1
+
+set +e
+
+pushd $(dirname ${BASH_SOURCE[0]})
+
+echo "Generating files..."
+dd if=/dev/urandom of=$DIR/test_1m.img bs=1K count=1K 2>&1 > /dev/null
+dd if=/dev/urandom of=$DIR/test_512m.img bs=1K count=512K 2>&1 > /dev/null
+touch $DIR/test_0b.img
+
+echo -e "\n.... Well-Formed Request Tests ...."
+
+echo "TEST: 1M file... "
+templates/wellformed_request.sh test_1m.img
+rm $DIR/test_1m.img
+
+echo "TEST: 512M file... "
+templates/wellformed_request.sh test_512m.img
+rm $DIR/test_512m.img
+
+echo "TEST: 0B file... "
+templates/wellformed_request.sh test_0b.img
+rm $DIR/test_0b.img
+
+echo -e "...................................\n"
+echo "Killing hypershare and cleaning up"
+
+kill -2 %1
+
+rm -r $DIR
+
+popd
